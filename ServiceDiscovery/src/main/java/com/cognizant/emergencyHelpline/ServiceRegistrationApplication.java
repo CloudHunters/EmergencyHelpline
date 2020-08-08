03d50f1ec3fd1e898dfe933fa45b15d7b56288c3
karthickRajendran03 <karthick.sacs@gmail.com>
2020-08-09 00:42:39 +0530

    added for discovery and gateway

--- ServiceDiscovery/src/main/java/com/cognizant/emergencyHelpline/ServiceRegistrationApplication.java
+++ ServiceDiscovery/src/main/java/com/cognizant/emergencyHelpline/ServiceRegistrationApplication.java
@@ -3,6 +3,7 @@ package com.cognizant.emergencyHelpline;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @EnableEurekaServer
 @SpringBootApplication
 public class ServiceRegistrationApplication {
